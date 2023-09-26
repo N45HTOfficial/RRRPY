7c119c9ae61c782f619e784ebe75ee28529b3e8c
N45HT <61760806+N45HT@users.noreply.github.com>
2023-09-26 20:23:16 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+# Use the PORT environment variable to specify the port
+port = int(os.environ.get("PORT", 5000))
+
+@app.route("/")
+def app_s():
+    return "Hello, World!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=port)
