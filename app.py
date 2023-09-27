ce31b3d379c3374f9f5c8281450b153270176007
Choirur Rizal <choirurrizal321@gmail.com>
2023-09-27 17:09:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,33 @@
 import os
-from flask import Flask
+import requests
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 port = int(os.environ.get("PORT", 8080))
 
 @app.route("/")
-def app_s():
+def index():
     return "Hello, World!"
+
+@app.route("/api/statusCheck", methods=["GET"])
+def status_check():
+    url = request.args.get("url")
+
+    if not url:
+        return jsonify({"error": "Missing 'url' parameter"}), 400
+    try:
+        response = requests.get(url)
+        status_code = response.status_code
+        response_data = {
+            "url": url,
+            "status_code": status_code,
+            "message": "Success" if response.ok else "Failed"
+        }
+        return jsonify(response_data), status_code
+
+    except requests.exceptions.RequestException as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=port)
